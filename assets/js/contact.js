30e2594631b6ea48c25441435c5eb8e60539de02
Hamaad-Siddiqui <hamaad.siddiqui404@gmail.com>
2020-07-10 17:26:17 +0500

    Fixed Form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,104 +1,87 @@
-$(document).ready(function(){
-    
-    (function($) {
-        "use strict";
+$(document).ready(function () {
+  (function ($) {
+    "use strict";
 
-    
-    jQuery.validator.addMethod('answercheck', function (value, element) {
-        return this.optional(element) || /^\bcat\b$/.test(value)
-    }, "type the correct answer -_-");
+    jQuery.validator.addMethod(
+      "answercheck",
+      function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value);
+      },
+      "type the correct answer -_-"
+    );
 
     // validate contactForm form
-    $(function() {
-        $('#contactForm').validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                subject: {
-                    required: true,
-                    minlength: 4
-                },
-                number: {
-                    required: true,
-                    minlength: 5
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                message: {
-                    required: true,
-                    minlength: 20
-                }
-            },
-            messages: {
-                name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
-                },
-                subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
-                },
-                number: {
-                    required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
-                },
-                email: {
-                    required: "no email, no message"
-                },
-                message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
-                }
-            },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    // url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
-                    },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
-                    }
-                })
-            }
-        })
-    })
-        
- })(jQuery)
-})
-
-
-// (function() {
-//     $('form > input').keyup(function() {
-
-//         var empty = false;
-//         $('form > input').each(function() {
-//             if ($(this).val() == '') {
-//                 empty = true;
-//             }
-//         });
-
-//         if (empty) {
-//             $('#register').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
-//         } else {
-//             $('#register').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
-//         }
-//     });
-// })()
+    $(function () {
+      $("#contactForm").validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2,
+          },
+          subject: {
+            required: true,
+            minlength: 4,
+          },
+          number: {
+            required: true,
+            minlength: 5,
+          },
+          email: {
+            required: true,
+            email: true,
+          },
+          message: {
+            required: true,
+            minlength: 20,
+          },
+        },
+        messages: {
+          name: {
+            required: "come on, you have a name, don't you?",
+            minlength: "your name must consist of at least 2 characters",
+          },
+          subject: {
+            required: "come on, you have a subject, don't you?",
+            minlength: "your subject must consist of at least 4 characters",
+          },
+          number: {
+            required: "come on, you have a number, don't you?",
+            minlength: "your Number must consist of at least 5 characters",
+          },
+          email: {
+            required: "no email, no message",
+          },
+          message: {
+            required:
+              "um...yea, you have to write something to send this form.",
+            minlength: "thats all? really?",
+          },
+        },
+        // submitHandler: function(form) {
+        //     $(form).ajaxSubmit({
+        //         type:"POST",
+        //         data: $(form).serialize(),
+        //         // url:"contact_process.php",
+        //         success: function() {
+        //             $('#contactForm :input').attr('disabled', 'disabled');
+        //             $('#contactForm').fadeTo( "slow", 1, function() {
+        //                 $(this).find(':input').attr('disabled', 'disabled');
+        //                 $(this).find('label').css('cursor','default');
+        //                 $('#success').fadeIn()
+        //                 $('.modal').modal('hide');
+        //             	$('#success').modal('show');
+        //             })
+        //         },
+        //         error: function() {
+        //             $('#contactForm').fadeTo( "slow", 1, function() {
+        //                 $('#error').fadeIn()
+        //                 $('.modal').modal('hide');
+        //             	$('#error').modal('show');
+        //             })
+        //         }
+        //     })
+        // }
+      });
+    });
+  })(jQuery);
+});
